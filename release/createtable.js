4375a5cb357383a7f3421ad0ac36d8fcb98f4b91
Alex Hanif <hanifbbz@gmail.com>
2012-09-16 00:01:00 +0200

    + improved template

--- release/createtable.js
+++ release/createtable.js
@@ -1,23 +1,25 @@
-for ( var h = 0; h < 24; h++ ) {
-	var ul = document.createElement( "ul" );
-	//add the hour number
-	var li = document.createElement( "li" );
-	li.id = h + "-hour1";
-	li.className = "hour";
-	li.textContent = h;
-	ul.appendChild( li );
-	//add all the minutes
-	for ( var m = 0; m < 60; m++ ) {
+$(document).ready(function(e) {
+	for ( var h = 0; h < 24; h++ ) {
+		var ul = document.createElement( "ul" );
+		//add the hour number
 		var li = document.createElement( "li" );
-		li.id = h + "-" + m;
+		li.id = h + "-hour1";
+		li.className = "hour";
+		li.textContent = h;
 		ul.appendChild( li );
+		//add all the minutes
+		for ( var m = 0; m < 60; m++ ) {
+			var li = document.createElement( "li" );
+			li.id = h + "-" + m;
+			ul.appendChild( li );
+		}
+		//add the trailing hour number
+		var li = document.createElement( "li" );
+		li.id = h + "-hour2";
+		li.className = "hour";
+		li.textContent = h;
+		ul.appendChild( li );
+		//add the ul to the container
+		ulContainer.appendChild( ul );
 	}
-	//add the trailing hour number
-	var li = document.createElement( "li" );
-	li.id = h + "-hour2";
-	li.className = "hour";
-	li.textContent = h;
-	ul.appendChild( li );
-	//add the ul to the container
-	ulContainer.appendChild( ul );
-}
+});
